85e7e8d4609bb2e4f9749921e1dacbe80e2de688
SirRaynald <raynaldapopo1@gmail.com>
2023-09-19 13:21:20 +0300

    authentication forms

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,6 +1,12 @@
 
-from django.urls import path,include
+from django.urls import path
 from . import views
+
+app_name = "testapp"
+
 urlpatterns = [
-    path('',views.home)
+    path('',views.home,name="home"),
+    path('about', views.about, name="about"),
+    path('login', views.login, name="login"),
+    path('register', views.register, name="register")
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 # Create your views he
 def home(request):
     return render(request,'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
